12b6e8ade1fb0fd56c361ea217b01e4e90dc778b
shubhrendusinha <62508970+shubhrendusinha@users.noreply.github.com>
2025-01-20 01:07:47 +0530

    fix syntax 9

--- main.tf
+++ main.tf
@@ -73,8 +73,8 @@ resource "aws_security_group_rule" "blog_http_in"{
   type="ingress"
   from_port=80
   to_port=80
-  protocol=tcp
-  cidr=["0.0.0.0/0"]
+  protocol="tcp"
+  cidr_blocks=["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
@@ -83,8 +83,8 @@ resource "aws_security_group_rule" "blog_https_in"{
   type="ingress"
   from_port=443
   to_port=443
-  protocol=tcp
-  cidr=["0.0.0.0/0"]
+  protocol="tcp"
+  cidr_blocks=["0.0.0.0/0"]
 
  security_group_id = aws_security_group.blog.id
 }
@@ -93,8 +93,8 @@ resource "aws_security_group_rule" "blog_everything_out"{
   type="egress"
   from_port=0
   to_port=0
-  protocol=-1
-  cidr=["0.0.0.0/0"]
+  protocol="-1"
+  cidr_blocks=["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
